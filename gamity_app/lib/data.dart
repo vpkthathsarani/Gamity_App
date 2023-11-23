82f181a1844cb4d8c0ebc1e3c97b1af71a6e9d47
vpkthathsarani <129584792+vpkthathsarani@users.noreply.github.com>
2023-11-24 01:44:31 +0530

    modify screen

--- gamity_app/lib/data.dart
+++ gamity_app/lib/data.dart
@@ -12,11 +12,11 @@ class Game {
   String description;
   String studio;
 
- 
+  
   Game(this.title, this.coverImage, this.description, this.studio)
       : images = []; 
 
-  
+ 
 }
 
 
@@ -24,33 +24,33 @@ List<Game> games = [
   Game(
     "Horizon Zero Dawn",
     GameImage(
-        "https://www.wallpaperflare.com/static/734/272/885/horizon-zero-dawn-playstation-4-logo-aloy-horizon-zero-dawn-wallpaper.jpg",
+        "https://images.pexels.com/photos/159393/gamepad-video-game-controller-game-controller-controller-159393.jpeg",
         ""),
-    "Description for Horizon Zero Dawn",
+    "",
     "Guerrilla Games",
   ),
   Game(
     "Metro Exodus",
     GameImage(
-        "https://assets1.ignimgs.com/2018/12/14/metro-exodus---button-1544750418985.jpg",
+        "https://images.pexels.com/photos/2885014/pexels-photo-2885014.jpeg",
         ""),
-    "Description for Metro Exodus",
+    "",
     "4A Games",
   ),
   Game(
     "Tom Clancy's The Division 2",
     GameImage(
-        "https://cdn.division.zone/uploads/2019/03/tc-the-division-2-preload-start-times-sizes-header.jpg",
+        "https://images.pexels.com/photos/159393/gamepad-video-game-controller-game-controller-controller-159393.jpeg",
         ""),
-    "Description for The Division 2",
+    "",
     "Massive Entertainment",
   ),
   Game(
     "Resident Evil 2",
     GameImage(
-        "https://media.playstation.com/is/image/SCEA/resident-evil-2-box-art-01-ps4-us-12dec18?\$native_nt\$",
+        "https://images.pexels.com/photos/159393/gamepad-video-game-controller-game-controller-controller-159393.jpeg",
         ""),
-    "Description for Resident Evil 2",
+    "",
     "Capcom",
   ),
 ];
@@ -59,31 +59,31 @@ List<Game> games2 = [
   Game(
     "Grand Theft Auto V",
     GameImage(
-        "https://gpstatic.com/acache/26/25/1/uk/packshot-7136bdab871d6b2c8f07ccc9ad33b4d0.jpg",
+        "https://images.pexels.com/photos/2885014/pexels-photo-2885014.jpeg",
         ""),
-    "Description for Grand Theft Auto V",
+    "",
     "Rockstar Games",
   ),
   Game(
     "The Last of Us Part II",
-    GameImage("https://mvpo.us/img/P6253.jpg", ""),
-    "Description for The Last of Us Part II",
+    GameImage("https://images.pexels.com/photos/2885014/pexels-photo-2885014.jpeg", ""),
+    "",
     "Naughty Dog",
   ),
   Game(
     "Sekiro: Shadows Die Twice",
     GameImage(
-        "https://onlysp.escapistmagazine.com/wp-content/uploads/2019/03/Sekiro-Shadows-Die-Twice-logo-art.jpg",
+        "https://images.pexels.com/photos/159393/gamepad-video-game-controller-game-controller-controller-159393.jpeg",
         ""),
-    "Description for Sekiro: Shadows Die Twice",
+    "",
     "From Software",
   ),
   Game(
     "Just Cause 4",
     GameImage(
-        "https://s3.gaming-cdn.com/images/products/2666/orig/just-cause-4-cover.jpg",
+        "https://images.pexels.com/photos/159393/gamepad-video-game-controller-game-controller-controller-159393.jpeg",
         ""),
-    "Description for Just Cause 4",
+    "",
     "Avalanche Studios",
   ),
 ];
@@ -92,32 +92,32 @@ List<Game> featuredGames = [
   Game(
     "Total War: Three Kingdoms",
     GameImage(
-        "https://content.totalwar.com/total-war/com.totalwar.www2019/uploads/2019/03/08145252/2.jpg",
+        "https://images.pexels.com/photos/2885014/pexels-photo-2885014.jpeg",
         ""),
-    "Description for Total War: Three Kingdoms",
+    "",
     "Feral Interactive",
   ),
   Game(
     "Call of Duty: Modern Warfare",
     GameImage(
-        "https://images.hdqwalls.com/download/call-of-duty-modern-warfare-2019-4k-cb-1920x1080.jpg",
+        "https://images.pexels.com/photos/159393/gamepad-video-game-controller-game-controller-controller-159393.jpeg",
         ""),
-    "Description for Call of Duty: Modern Warfare",
+    "",
     "Infinity Ward",
   ),
   Game(
     "Dragon Ball Z: Kakarot",
     GameImage(
-        "https://cdn3.dualshockers.com/wp-content/uploads/2020/01/dbx-kakarot.png",
+        "https://images.pexels.com/photos/159393/gamepad-video-game-controller-game-controller-controller-159393.jpeg",
         ""),
-    "Description for Dragon Ball Z: Kakarot",
+    "",
     "CyberConnect2",
   ),
   Game(
     "Mortal Kombat 11",
     GameImage(
-        "https://static.trueachievements.com/customimages/093902.jpg", ""),
-    "Description for Mortal Kombat 11",
+        "https://images.pexels.com/photos/159393/gamepad-video-game-controller-game-controller-controller-159393.jpeg", ""),
+    "",
     "NetherRealm Studios",
-  ),
+  )
 ];
--- gamity_app/lib/pages/home_page.dart
+++ gamity_app/lib/pages/home_page.dart
@@ -1,50 +1,124 @@
 import 'package:flutter/material.dart';
+import '../data.dart';  // Import the file where featuredGames is defined
 
-import '../data.dart';
-
-
-
-class HomePage extends StatefulWidget{
-  
-
+class HomePage extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
     return _HomePageState();
-
   }
 }
 
-class _HomePageState extends State<HomePage>{
-
+class _HomePageState extends State<HomePage> {
   var _deviceHeight;
   var _deviceWidth;
 
   @override
-  void initState() {
-    super.initState();
-    
-  }
-
-  @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     _deviceHeight = MediaQuery.of(context).size.height;
     _deviceWidth = MediaQuery.of(context).size.width;
     return Scaffold(
       body: Stack(
-        children: <Widget>[_featuredGamesWidget()],),
+        children: <Widget>[
+          _featuredGamesWidget(),
+          _gradientBoxWidget(),
+          _topLayerWidget(),
+        ],
+      ),
     );
   }
 
-
-   Widget _featuredGamesWidget() {
+  Widget _featuredGamesWidget() {
     return SizedBox(
+      child: Container(
         height: _deviceHeight * 0.50,
         width: _deviceWidth,
-        child: Container(
-          decoration: BoxDecoration(
-            image: DecorationImage(image: NetworkImage(featuredGames[0].coverImage.url),),
-          ),
-        )
+        child: PageView(
+          scrollDirection: Axis.horizontal,
+          children: featuredGames.map((_game) {
+            return Container(
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  fit: BoxFit.cover,
+                  image: NetworkImage(_game.coverImage.url),
+                ),
+              ),
+            );
+          }).toList(),
+        ),
+      ),
     );
-}
+  }
+
+  Widget _gradientBoxWidget() {
+    return Align(
+      alignment: Alignment.bottomCenter,
+      child: Container(
+        height: _deviceHeight * 0.80,
+        width: _deviceWidth,
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              Color.fromRGBO(35, 45, 59, 1.0),
+              Colors.transparent,
+            ],
+            stops: [0.65, 1.0],
+            begin: Alignment.bottomCenter,
+            end: Alignment.topCenter,
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _topLayerWidget() {
+    return Padding(
+      padding: EdgeInsets.symmetric(
+        horizontal: _deviceWidth * 0.05,
+        vertical: _deviceWidth * 0.005,
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        mainAxisSize: MainAxisSize.max,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          _topBarWidget(),
+        ],
+      ),
+    );
+  }
+
+  Widget _topBarWidget() {
+    return SizedBox(
+      height: _deviceHeight * 0.13,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        mainAxisSize: MainAxisSize.max,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          Icon(
+            Icons.menu,
+            color: Colors.white,
+            size: 30,
+          ),
+          Row(
+            children: <Widget>[
+              Icon(
+                Icons.search,
+                color: Colors.white,
+                size: 30,
+              ),
+              SizedBox(
+                width: _deviceWidth * 0.03,
+              ),
+              Icon(
+                Icons.notifications_none,
+                color: Colors.white,
+                size: 30,
+              ),
+            ],
+          )
+        ],
+      ),
+    );
+  }
 }
